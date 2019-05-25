64a0992262dbfe246c792642c3f8eff7cd72e694
sineth97 <47888753+sineth97@users.noreply.github.com>
2019-05-25 23:08:06 +0530

    Add files via upload

---
+++ SellNBuy1/src/main/java/com/paf/sellnbuy/model/Order.java
@@ -0,0 +1,74 @@
+package com.paf.sellnbuy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int oid;
+	private String odate;
+	private int qty;
+	private double total;
+	private int fid;
+	private int cid;
+	private int sid;
+	
+	
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public String getOdate() {
+		return odate;
+	}
+	public void setOdate(String odate) {
+		this.odate = odate;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Order [oid=" + oid + ", odate=" + odate + ", qty=" + qty + ", total=" + total + ", fid=" + fid
+				+ ", cid=" + cid + ", sid=" + sid + "]";
+	}
+	
+}
